384d64adbba4be8ac4ac0502664e49b4036dcdf5
Nick Cassano <romanlaughs@gmail.com>
2022-11-30 21:00:54 -0800

    Added testing and stress testing package as well as MySQL Optimizations from limiting promises to 1

--- index.js
+++ index.js
@@ -11,38 +11,43 @@ const port = 3000;
 app.use(bodyParser.json())
 
 
-app.get('/products/:product_id', (req, res) => {
-  var dataPromises = [];
-  var product_id = Number(req.params.product_id);
-  var productQuery = `SELECT * FROM PRODUCTS WHERE (id = ${product_id})`;
-  var featureQuery = `SELECT * FROM FEATURES WHERE (feats_ref_Id = ${product_id})`;
-  var productData = db.queryAsync(productQuery);
-  var featureData = db.queryAsync(featureQuery);
-  dataPromises.push(productData);
-  dataPromises.push(featureData);
+// app.get('/products/:product_id', (req, res) => {
+//   var dataPromises = [];
+//   var product_id = Number(req.params.product_id);
+//   var productQuery = `SELECT * FROM PRODUCTS WHERE (id = ${product_id})`;
+//   var featureQuery = `SELECT * FROM FEATURES WHERE (feats_ref_Id = ${product_id})`;
+//   var productData = db.queryAsync(productQuery);
+//   var featureData = db.queryAsync(featureQuery);
+//   dataPromises.push(productData);
+//   dataPromises.push(featureData);
 
-  Promise.all(dataPromises)
-    .then((results) => {
-      var resultObj = {};
-      var product = results[0][0][0];
-      var featureList = results[1][0];
-      resultObj.id = product.id;
-      resultObj.name = product.name;
-      resultObj.slogan = product.slogan;
-      resultObj.description = product.description;
-      resultObj.category = product.category;
-      resultObj.default_price = '' + product.default_price + '.00';
-      resultObj.features = [];
+//   Promise.all(dataPromises)
+//     .then((results) => {
+//       var resultObj = {};
+//       var product = results[0][0][0];
+//       var featureList = results[1][0];
+//       resultObj.id = product.id;
+//       resultObj.name = product.name;
+//       resultObj.slogan = product.slogan;
+//       resultObj.description = product.description;
+//       resultObj.category = product.category;
+//       resultObj.default_price = '' + product.default_price + '.00';
+//       resultObj.features = [];
 
-      for (var i = 0; i < featureList.length; i++) {
-        var container = {};
-        container.feature = featureList[i].feature;
-        container.value = featureList[i].value;
-        resultObj.features.push(container);
-      }
-      res.send(resultObj);
-    })
-})
+//       for (var i = 0; i < featureList.length; i++) {
+//         var container = {};
+//         container.feature = featureList[i].feature;
+//         container.value = featureList[i].value;
+//         resultObj.features.push(container);
+//       }
+//       res.send(resultObj);
+//     })
+// })
+
+//****************************************** */
+// REFRACTOR TO MAKE ONE DATA CALL FROM DB
+// select * from styles, photos, skus where styles.style_ref_id = 1 and photos.photos_ref_Id = styles.id and skus.skus_ref_Id = styles.id;
+//****************************************** */
 
 app.get('/products/:product_id/styles', (req, res) => {
   var dataPromises = [];
@@ -52,60 +57,90 @@ app.get('/products/:product_id/styles', (req, res) => {
   dataPromises.push(styleData);
   Promise.all(dataPromises)
     .then((results) => {
-      dataPromises = [];
-      var styles = results[0][0]
-      var ref_id = styles[0].style_ref_Id;
-      var photoQuery = `SELECT * FROM PHOTOS WHERE (photos_ref_Id = ${ref_id})`;
-      var skuQuery = `SELECT * FROM SKUS WHERE (skus_ref_Id = ${ref_id})`;
-      var photoData = db.queryAsync(photoQuery)
-      var skuData = db.queryAsync(skuQuery)
-      dataPromises.push(photoData);
-      dataPromises.push(skuData);
+      if (results[0][0].length <= 0) {
+        res.send('No Other Styles Found for This Item')
+      } else {
+        dataPromises = [];
+        var styles = results[0][0]
+        var ref_id = styles[0].style_ref_Id;
+        var photoQuery = `SELECT * FROM PHOTOS WHERE (photos_ref_Id = ${ref_id})`;
+        var skuQuery = `SELECT * FROM SKUS WHERE (skus_ref_Id = ${ref_id})`;
+        var photoData = db.queryAsync(photoQuery)
+        var skuData = db.queryAsync(skuQuery)
+        dataPromises.push(photoData);
+        dataPromises.push(skuData);
 
-      Promise.all(dataPromises)
-        .then((resultsArray) => {
-          var resultObj = {};
-          resultObj.product_id = '' + product_id;
-          resultObj.results = [];
-          for (var l = 0; l < styles.length; l++) {
-            var styleObj = {}
-            styleObj.style_id = styles[l].id;
-            styleObj.name = styles[l].name;
-            styleObj.original_price = '' + styles[l].original_price + '.00'
-            if (styles[l].sale_price === undefined) {
-              styleObj.sale_price = null
-            } else {
-              styleObj.sale_price = '' + styles[l].sale_price + '.00'
+        Promise.all(dataPromises)
+          .then((resultsArray) => {
+            var resultObj = {};
+            resultObj.product_id = '' + product_id;
+            resultObj.results = [];
+            for (var l = 0; l < styles.length; l++) {
+              var styleObj = {}
+              styleObj.style_id = styles[l].id;
+              styleObj.name = styles[l].name;
+              styleObj.original_price = '' + styles[l].original_price + '.00'
+              if (styles[l].sale_price === 'null') {
+                styleObj.sale_price = null
+              } else {
+                styleObj.sale_price = '' + styles[l].sale_price + '.00'
+              }
+              if (styles[l].isDefault > 0) {
+                styleObj['default?'] = true;
+              } else {
+                styleObj['default?'] = false;
+              }
+              styleObj.photos = [];
+              styleObj.skus = {};
+              var photos = resultsArray[0][0];
+              var skus = resultsArray[1][0];
+              for (var i = 0; i < photos.length; i++) {
+                var container = {};
+                container.thumbnail_url = photos[i].thumbnail_url;
+                container.url = photos[i].url;
+                styleObj.photos.push(container);
+              }
+              for (var j = 0; j < skus.length; j++) {
+                var sku_id = skus[j].id;
+                styleObj.skus[sku_id] = {}
+                styleObj.skus[sku_id].quantity = skus[j].quantity;
+                styleObj.skus[sku_id].size = skus[j].size;
+              }
+              resultObj.results.push(styleObj);
             }
-            if (styles[l].isDefault > 0) {
-              styleObj['default?'] = true;
-            } else {
-              styleObj['default?'] = false;
-            }
-            styleObj.photos = [];
-            styleObj.skus = {};
-            var photos = resultsArray[0][0];
-            var skus = resultsArray[1][0];
-            for (var i = 0; i < photos.length; i++) {
-              var container = {};
-              container.thumbnail_url = photos[i].thumbnail_url;
-              container.url = photos[i].url;
-              styleObj.photos.push(container);
-            }
-            for (var j = 0; j < skus.length; j++) {
-              var sku_id = skus[j].id;
-              styleObj.skus[sku_id] = {}
-              styleObj.skus[sku_id].quantity = skus[j].quantity;
-              styleObj.skus[sku_id].size = skus[j].size;
-            }
-            resultObj.results.push(styleObj);
-          }
 
-          res.send(resultObj);
-        })
-
+            res.send(resultObj);
+          })
+      }
     })
+})
 
+app.get('/products/:product_id', (req, res) => {
+  var dataPromises = [];
+  var product_id = Number(req.params.product_id);
+  var testQuery = `select * from products, features where products.id = ${product_id} AND features.feats_ref_Id = products.id`
+  var productData = db.queryAsync(testQuery);
+  dataPromises.push(productData);
+  Promise.all(dataPromises)
+    .then((results) => {
+      var resultObj = {};
+      var product = results[0][0][0];
+      var featureList = results[0][0];
+      resultObj.id = product.id;
+      resultObj.name = product.name;
+      resultObj.slogan = product.slogan;
+      resultObj.description = product.description;
+      resultObj.category = product.category;
+      resultObj.default_price = '' + product.default_price + '.00';
+      resultObj.features = [];
+      for (var i = 0; i < featureList.length; i++) {
+        var container = {};
+        container.feature = featureList[i].feature;
+        container.value = featureList[i].value;
+        resultObj.features.push(container);
+      }
+      res.send(resultObj);
+    })
 })
 
 app.listen(port, () => {
--- test/test.js
+++ test/test.js
@@ -1,11 +1,73 @@
 var chai = require('chai');
+var chaiHttp = require('chai-http');
+
+chai.use(chaiHttp);
 
 var assert = chai.assert;
 
 describe('Array', function() {
   it('should start empty', function() {
     var arr = [];
-
     assert.equal(arr.length, 0);
   });
 });
+
+describe('Products API', function() {
+  it('SHOULD RETURN CORRECT DATA FROM API FOR PRODUCT_ID 1', () => {
+    chai.request('http://localhost:3000')
+      .get('/products/1')
+      .end((err, res) => {
+        assert.equal(res.status, 200, 'status is 200');
+        assert.equal(typeof res.body, 'object', 'it returns an object');
+        assert.equal(res.body.id, 1, 'it returns the correct product_id');
+        assert.equal(res.body.name, 'Camo Onesie');
+        assert.equal(res.body.features[0].feature, 'Fabric');
+      })
+  })
+  it('SHOULD RETURN CORRECT DATA FROM API FOR PRODUCT_ID 1000', () => {
+    chai.request('http://localhost:3000')
+      .get('/products/1000')
+      .end((err, res) => {
+        assert.equal(res.status, 200, 'status is 200');
+        assert.equal(typeof res.body, 'object', 'it returns an object');
+        assert.equal(res.body.id, 1000, 'it returns the correct product_id');
+        assert.equal(res.body.name, 'Katlynn Jacket');
+        assert.equal(res.body.features[0].feature, 'Cut');
+      })
+  })
+})
+
+describe('Styles API', function() {
+  it('SHOULD RETURN CORRECT DATA FROM API FOR PRODUCT_ID 1', () => {
+    chai.request('http://localhost:3000')
+      .get('/products/1/styles')
+      .end((err, res) => {
+        assert.equal(res.status, 200, 'status is 200');
+        assert.equal(typeof res.body, 'object', 'it returns an object');
+        assert.equal(res.body.product_id, '1', 'it returns the correct product_id');
+        assert.equal(res.body.results[0].name, 'Forest Green & Black');
+        assert.equal(res.body.results[0].photos[0].url, '\"https://images.unsplash.com/photo-1501088430049-71c79fa3283e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80\"');
+        assert.equal(res.body.results[0].skus['1'].quantity, '8');
+      })
+  })
+  it('SHOULD RETURN CORRECT DATA FROM API FOR PRODUCT_ID 1002', () => {
+    chai.request('http://localhost:3000')
+      .get('/products/1002/styles')
+      .end((err, res) => {
+        assert.equal(res.status, 200, 'status is 200');
+        assert.equal(typeof res.body, 'object', 'it returns an object');
+        assert.equal(res.body.product_id, '1002', 'it returns the correct product_id');
+        assert.equal(res.body.results[0].name, 'Orange');
+        assert.equal(res.body.results[0].photos[0].url, '\"https://images.unsplash.com/photo-1519862170344-6cd5e49cb996?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80\"');
+        assert.equal(res.body.results[0].skus['5822'].quantity, '50');
+      })
+  })
+  it('SHOULD RETURN MESSAGE FROM API FOR PRODUCTS WITH NO STYLES', () => {
+    chai.request('http://localhost:3000')
+      .get('/products/1000/styles')
+      .end((err, res) => {
+        assert.equal(res.status, 200, 'status is 200');
+        assert.equal(typeof res.body, 'object', 'it returns an object');
+      })
+  })
+})
